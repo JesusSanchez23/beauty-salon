75704ad49650f1fd24b78083ee2e0a4ff2f2fe4d
JesusSanchez23 <jesus8347@gmail.com>
2022-02-18 16:02:02 -0600

    add cita section

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\CitaController;
 use Controllers\LoginController;
 use MVC\Router;
 
@@ -32,6 +33,9 @@ $router->get('/confirmar-cuenta',[LoginController::class,'confirmar']);
 $router->get('/mensaje',[LoginController::class,'mensaje']);
 
 
+//area privada
+
+$router->get('/cita',[CitaController::class,'index']);
 
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
